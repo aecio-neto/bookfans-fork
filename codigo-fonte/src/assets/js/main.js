b85a5ac369e6f587b97f741eb6121fcd6b1641d2
Aécio Neto <aecyoneto@gmail.com>
2024-11-11 11:27:07 -0300

    remove ; from js

--- codigo-fonte/src/assets/js/main.js
+++ codigo-fonte/src/assets/js/main.js
@@ -1,14 +1,14 @@
 document.querySelectorAll('.star-rating').forEach(starRating => {
   // Cria um array a partir do NodeList
-  const stars = Array.from(starRating.querySelectorAll('.star')).reverse();
+  const stars = Array.from(starRating.querySelectorAll('.star')).reverse()
   // Reverse acontece por conta do css ~ siblings operator. 
   
   stars.forEach((star, index) => {
     star.addEventListener('click', () => {
       // Loop adiciona click/class. 
       stars.forEach((hoveredStar, i) => {
-        hoveredStar.classList.toggle('selected', i <= index);
-      });
-    });
-  });
-});
+        hoveredStar.classList.toggle('selected', i <= index)
+      })
+    })
+  })
+})
