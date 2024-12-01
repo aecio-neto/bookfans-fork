bf87915ff37cc58eeed9a686b037a8cfca7a0155
LeopoldoFonseca <leo.cienciadedados@gmail.com>
2024-12-01 16:41:15 -0300

    Atualizações

--- codigo-fonte/src/paginaFeed/feed.js
+++ codigo-fonte/src/paginaFeed/feed.js
@@ -1,3 +1,20 @@
+// criar post
+document.addEventListener('DOMContentLoaded', function () {
+    const postButton = document.getElementById('postar');
+    postButton.addEventListener('click', postarMensagem);
+
+    function postarMensagem() {
+        const textarea = document.getElementById('my-textarea');
+        const texto = textarea.value;
+        const postMade = document.getElementById('post-made');
+        const postContent = postMade.querySelector('.post-content');
+        postContent.textContent = texto;
+        postMade.style.display = 'block'; // Mostrar o elemento post-made inteiro
+        textarea.value = '';
+        document.getElementById('ipost-user').textContent = usuarioLogado.email;
+    }
+});
+
 // Obtém a data e hora atual
 const dataAtual = new Date();
 
@@ -15,28 +32,3 @@ postDateElement.textContent = dataFormatada;
 // Associar eventos aos botões
 const botaoPostar = document.getElementById('postar');
 botaoPostar.addEventListener('click', postarMensagem);
-
-// criar post
-document.addEventListener('DOMContentLoaded', function () {
-        const postButton = document.getElementById('postar');
-        postButton.addEventListener('click', postarMensagem);
-
-        function postarMensagem() {
-                const textarea = document.getElementById('my-textarea');
-                const texto = textarea.value;
-                const postMade = document.getElementById('post-made');
-                const postContent = postMade.querySelector('.post-content');
-                postContent.textContent = texto;
-                postMade.style.display = 'block'; // Mostrar o elemento post-made inteiro
-                textarea.value = '';
-                document.getElementById('ipost-user').textContent = usuarioLogado.email;
-        }
-});
-
-const usuarioLogado = JSON.parse(localStorage.getItem('usuarios'))[0];
-if (usuarioLogado) {
-    document.getElementById('nome-usuario').textContent = usuarioLogado.email;
-} else {
-    console.error('Usuário não encontrado');
-    // Ou exibir uma mensagem para o usuário
-}
